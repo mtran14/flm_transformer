8d2e3b45090af6ecb9c07b1c1a8116890b919f3e
mtran14 <mtran14@u.rochester.edu>
2020-12-16 21:03:50 +0000

    fix variables for godz

--- preprocess/parallel_preprocessing.py
+++ preprocess/parallel_preprocessing.py
@@ -2,16 +2,16 @@ import os
 import sys
 
 #--input_path ../data/openface_vox_sample/ --output_path ../data/openface_npy/ --chunk 3 --n_chunk 16
-input_path = "/data/perception-temp/vox2_crop_fps25/"
-output_path = "/data/perception-temp/vox2/vox2_npy/"
+input_path = "/shares/perception-working/minh/openface_voxceleb2/"
+output_path = "/shares/perception-working/minh/vox2/vox2_npy/"
 #input_path = "../data/openface_vox_sample/"
 #output_path = "../data/openface_npy/"
 window_size = 5
 step_size = 2
 downsamping_rate = 1
 
-current_chunk = sys.argv[1]
-n_chunk = sys.argv[2]
+current_chunk = sys.argv[2]
+n_chunk = sys.argv[1]
 
 cmd = "taskset --cpu-list " + current_chunk + \
     " python preprocess_openface.py --input_path " +input_path + \
