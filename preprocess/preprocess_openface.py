fa68b40282da544ae6568dd709c1ae6845375e1e
nhatminh886688 <mtran14@u.rochester.edu>
2020-12-21 22:30:56 -0500

    code to preprocess openface

--- preprocess/preprocess_openface.py
+++ preprocess/preprocess_openface.py
@@ -81,12 +81,18 @@ def openface_preprocess(args):
     file_list = os.listdir(input_path)
     chunk_size = len(file_list) // args.n_chunk
     current_files = file_list[chunk_size * args.chunk : chunk_size * (args.chunk+1)]
-    overall_file_outname = "train-clean-360_chunk_"+str(args.chunk) + ".csv"
+    overall_file_outname = "train-clean-schz_chunk_"+str(args.chunk) + ".csv"
     overall_clean_data = []
     for file in current_files:
         file_path = os.path.join(input_path, file)
         data = pd.read_csv(file_path).values
         data = data[::args.dr]
+        drops = []
+        for k in range(data.shape[0]):
+            for element in data[k]:
+                if(isinstance(element, str)):
+                    drops.append(k)
+        data = np.delete(data, drops, axis=0)
         if(data.shape[0] == 0):
             continue
         data = sliding_window(data, args.window_size, args.step_size)
@@ -94,12 +100,12 @@ def openface_preprocess(args):
         for j in range(data.shape[0]):
             X = []
             y = []
-            if(data[j][3] < 0.9):
+            if(data[j][2] < 0.9): #3
                 continue
             if(args.nose_al == 1):
-                for i in [27,28,29,30]:
-                    x_val_index = 5 + i
-                    y_val_index = 73 + i           
+                for i in [27,28,29,30]: #5 and 73
+                    x_val_index = 16 + i
+                    y_val_index = 84 + i           
                     X.append(data[j][x_val_index])
                     y.append(data[j][y_val_index])
                 #try:
@@ -111,13 +117,15 @@ def openface_preprocess(args):
                 new_X = []
                 new_Y = []
                 for i in range(68):
-                    x_val_index = 5 + i
-                    y_val_index = 73 + i  
+                    x_val_index = 16 + i
+                    y_val_index = 84 + i  
                     origin = (0,0)
                     qx, qy = rotate(origin, (data[j][x_val_index],data[j][y_val_index]), rotate_angle)
                     new_X.append(qx)
                     new_Y.append(qy)
                 
+                new_X = np.array(new_X)
+                new_Y = np.array(new_Y)
                 new_X -= min(new_X)
                 new_Y -= min(new_Y)
                 
@@ -132,7 +140,7 @@ def openface_preprocess(args):
                     new_row = rotate_frame(new_row)  
                 new_row = np.array(new_row)
                 nr_X = new_row[0:68]
-                nr_Y = new_row[68:]
+                nr_Y = new_row[68:136]
                 nr_X -= np.mean(nr_X)
                 nr_X /= 300
                 nr_Y -= np.mean(nr_Y)
@@ -143,8 +151,8 @@ def openface_preprocess(args):
                 new_X = []
                 new_Y = []
                 for i in range(68):
-                    x_val_index = 5 + i
-                    y_val_index = 73 + i
+                    x_val_index = 16 + i
+                    y_val_index = 84 + i
                     new_X.append(data[j][x_val_index])
                     new_Y.append(data[j][y_val_index]) 
                 new_X -= min(new_X)
@@ -155,7 +163,7 @@ def openface_preprocess(args):
                 new_row = list(new_X)+list(new_Y) 
                 new_row = np.array(new_row)
                 nr_X = new_row[0:68]
-                nr_Y = new_row[68:]
+                nr_Y = new_row[68:136]
                 nr_X -= np.mean(nr_X)
                 nr_X /= 300
                 nr_Y -= np.mean(nr_Y)
