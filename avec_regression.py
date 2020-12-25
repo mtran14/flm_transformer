6543460fdb82e3b1705fea81f5a769826b690747
nhatminh886688 <mtran14@u.rochester.edu>
2020-12-24 22:44:44 -0500

    fix init

--- avec_regression.py
+++ avec_regression.py
@@ -92,8 +92,8 @@ if(pretrain_option):
         config = {
                     'mode'     : 'regression',
                     'sample_rate' : 1,
-                    'hidden_size'       : 128,
-                    'pre_linear_dims'       : [20], 'post_linear_dims': [20],'drop':0.2,
+                    'hidden_size'       : 728,
+                    'pre_linear_dims'       : [100,50], 'post_linear_dims': [100,50],'drop':0.2,
                     'concat': 1, 'layers': 3, 'linear': False,
                 }        
         options = {
