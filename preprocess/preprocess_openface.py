27120839d1a8e25bc076e461e166e5aaf5d8d77e
nhatminh886688 <mtran14@u.rochester.edu>
2020-12-26 21:47:01 -0500

    code to preprocess vox

--- preprocess/preprocess_openface.py
+++ preprocess/preprocess_openface.py
@@ -103,9 +103,9 @@ def openface_preprocess(args):
             if(data[j][3] < 0.9): #3 for VoxCeleb, 2 for Schz
                 continue
             if(args.nose_al == 1):
-                for i in [27,28,29,30]: #5 and 73 for VoxCeleb; 16 and 84 for Schz
-                    x_val_index = 299 + i
-                    y_val_index = 367 + i           
+                for i in [27,28,29,30]: #5 and 73 for VoxCeleb; 16 and 84 for Schz, 299 and 367 for Avec
+                    x_val_index = 5 + i
+                    y_val_index = 73 + i           
                     X.append(data[j][x_val_index])
                     y.append(data[j][y_val_index])
                 #try:
@@ -117,8 +117,8 @@ def openface_preprocess(args):
                 new_X = []
                 new_Y = []
                 for i in range(68):
-                    x_val_index = 299 + i
-                    y_val_index = 367 + i  
+                    x_val_index = 5 + i
+                    y_val_index = 73 + i  
                     origin = (0,0)
                     qx, qy = rotate(origin, (data[j][x_val_index],data[j][y_val_index]), rotate_angle)
                     new_X.append(qx)
