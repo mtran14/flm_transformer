fa84ed8c16e0ef182a51441af5c0758a571a62a0
nhatminh886688 <mtran14@u.rochester.edu>
2020-12-28 17:24:34 -0500

    fix bug

--- avec_regression.py
+++ avec_regression.py
@@ -275,7 +275,7 @@ for seed in seeds:
             chosen_stats = dev_test_scores[min(dev_test_scores)]
             chosen_dev_scores = dev_score_break_down[min(dev_test_scores)]
             print("BEST PERFORMING SCORES: ", model_name, chosen_stats)
-            output.append([seed, model_name, chosen_dev_scores[0], chosen_dev_scores[1], chosen_stats[0], chosen_stats[1]])
+            output.append([seed, model_name, chosen_dev_scores[0].item(), chosen_dev_scores[1].item(), chosen_stats[0].item(), chosen_stats[1].item()])
 
     pretrain_option = False
     if(not pretrain_option):
@@ -422,7 +422,7 @@ for seed in seeds:
         chosen_stats = dev_test_scores[min(dev_test_scores)]
         chosen_dev_scores = dev_score_break_down[min(dev_test_scores)]
         print("BEST PERFORMING SCORES: ", chosen_stats)
-        output.append([seed, "N/A", chosen_dev_scores[0], chosen_dev_scores[1], chosen_stats[0], chosen_stats[1]])
+        output.append([seed, "N/A", chosen_dev_scores[0].item(), chosen_dev_scores[1].item(), chosen_stats[0].item(), chosen_stats[1].item()])
 pd.DataFrame(output).to_csv("avec_seed_results.csv", header=None, index=False)
             
 
