4acebd1b40281ac75c66e81b9b91249d8dfd8732
nhatminh886688 <mtran14@u.rochester.edu>
2021-01-11 18:58:37 -0500

    fun stuff

--- avec_regression.py
+++ avec_regression.py
@@ -45,7 +45,7 @@ def concordance_correlation_coefficient(y_true, y_pred,
     return numerator/denominator
 
 output = []
-subset = ["au"]
+subset = ["gp", "au"]
 #model_name_flm = "../GoogleDrive/flm_models/states-250000.ckpt"
 #model_name_au = "../GoogleDrive/flm_models/au_base.ckpt"
 #model_name_gp = "../GoogleDrive/flm_models/gp_base.ckpt"
