7a7d62f319ccdc11d51ec3e67412bdfa284d2909
nhatminh886688 <mtran14@u.rochester.edu>
2021-01-14 18:47:13 -0500

    fix bug, freeze pt

--- classifier.py
+++ classifier.py
@@ -90,7 +90,7 @@ model_name_dict = {"flm":model_name_flm, "au":model_name_au, "gp":model_name_gp,
 seeds = list(np.random.randint(0,1000,5))
 drugconds = ["PL","OT"]
 pretrain_option = [True,False]
-sources = ["gp", "au"]
+sources = ["gpau"]
 
 output = []
 for seed in seeds:
